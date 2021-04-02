2a4120d6e31fe9d272b2ff17fea1a2ec3100f4d0
hannan-abdul <shohas563@gmail.com>
2021-04-02 21:25:49 +0600

    another updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ko6w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollection = client.db("onlineStore").collection("onlineProducts");
